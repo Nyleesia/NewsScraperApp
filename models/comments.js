e81bd9324c1e6e5f1ba6b3336f23df332fdd3d01
Nyleesia Nicome Schnarr <nyleesia.nicome@users.noreply.github.com>
2019-10-22 11:21:04 -0400

    models/comments schema

---
+++ models/comments.js
@@ -0,0 +1,24 @@
+//Get dependency
+const mongoose = require("mongoose");
+
+// Setup new schema to handle comments
+const Schema = mongoose.Schema;
+const CommentSchema = new Schema({
+  name: {
+    type: String,
+  },
+  body: {
+    type: String,
+    required: true
+  },
+  comment: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ]
+});
+const Comment = mongoose.model("CCmment", CommentSchema);
+
+//Export Comment Schema
+module.exports = Comment;
